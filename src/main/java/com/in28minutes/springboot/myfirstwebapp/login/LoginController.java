5362d63a57087d0dd29b95d52b138a65fd59a8e7
noyechan <noy3928@naver.com>
2023-10-12 09:11:05 +0900

    step 11

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,22 +1,13 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class LoginController {
-
-    ///login => com.in28minutes.springboot.myfirstwebapp.login.LoginController => login.jsp
-
-    //http://localhost:8080/login?name=Ranga
-    //Model
+    //login
     @RequestMapping("login")
-    public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-        model.put("name", name);
-        System.out.println("Request param is " + name); //NOT RECOMMENDED FOR PROD CODE
+    public String gotoLoginPage() {
         return "login";
     }
 }
